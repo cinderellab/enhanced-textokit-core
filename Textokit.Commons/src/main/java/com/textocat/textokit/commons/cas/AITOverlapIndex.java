48ae7c97122a578d3f6228f57abc47fbd2673172
cinderellab <cinderela80@proton.me>
2021-02-21 10:09:54 +0000

    workshop

--- Textokit.Commons/src/main/java/com/textocat/textokit/commons/cas/AITOverlapIndex.java
+++ Textokit.Commons/src/main/java/com/textocat/textokit/commons/cas/AITOverlapIndex.java
@@ -14,4 +14,47 @@
  *    limitations under the License.
  */
 
-package com.textocat.tex
+package com.textocat.textokit.commons.cas;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.textocat.textokit.commons.util.OffsetsWithValue;
+import org.apache.uima.cas.text.AnnotationFS;
+
+import java.util.*;
+
+/**
+ * @author Rinat Gareev
+ */
+class AITOverlapIndex<A extends AnnotationFS> implements OverlapIndex<A> {
+    private ImmutableAugmentedIntervalTree<A> tree;
+
+    AITOverlapIndex(Iterator<A> srcIter) {
+        List<A> srcList = Lists.newArrayList(srcIter);
+        Collections.sort(srcList, offsetComp);
+        List<OffsetsWithValue<A>> intervals = Lists.transform(srcList, new Function<A, OffsetsWithValue<A>>() {
+            @Override
+            public OffsetsWithValue<A> apply(A anno) {
+                return new OffsetsWithValue<A>(anno.getBegin(), anno.getEnd(), anno);
+            }
+        });
+        tree = new ImmutableAugmentedIntervalTree<>(intervals);
+    }
+
+    @Override
+    public Set<A> getOverlapping(int begin, int end) {
+        Set<A> result = Sets.newLinkedHashSet();
+        List<OffsetsWithValue<A>> resultList = tree.getOverlapping(begin, end);
+        result.addAll(Lists.transform(resultList, new Function<OffsetsWithValue<A>, A>() {
+            @Override
+            public A apply(OffsetsWithValue<A> input) {
+                return input.getValue();
+            }
+        }));
+        return result;
+    }
+
+    private final Comparator<AnnotationFS> offsetComp = AnnotationOffsetComparator
+            .instance(AnnotationFS.class);
+}
