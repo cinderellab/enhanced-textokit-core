ff316a1a276ad14560fe3c8e751aee93d8fb9ae8
cinderellab <cinderela80@proton.me>
2021-03-15 20:26:42 +0000

    Update dbc2.html

--- Textokit.Commons/src/main/java/com/textocat/textokit/commons/cli/FileValueValidator.java
+++ Textokit.Commons/src/main/java/com/textocat/textokit/commons/cli/FileValueValidator.java
@@ -5,4 +5,34 @@
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
  *
- *   
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+package com.textocat.textokit.commons.cli;
+
+import com.beust.jcommander.IValueValidator;
+import com.beust.jcommander.ParameterException;
+
+import java.io.File;
+
+/**
+ * @author Rinat Gareev
+ */
+public class FileValueValidator implements IValueValidator<File> {
+
+    @Override
+    public void validate(String name, File value) throws ParameterException {
+        if (!value.isFile()) {
+            throw new ParameterException(String.format(
+                    "%s is not an existing file", value));
+        }
+    }
+
+}
---
+++ Textokit.Commons/src/main/java/com/textocat/textokit/commons/consumer/AnnotationPerLineWriter.java
@@ -0,0 +1,36 @@
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.commons.consumer;
+
+import com.textocat.textokit.commons.io.IoUtils;
+import com.textocat.textokit.commons.util.AnnotatorUtils;
+import com.textocat.textokit.commons.util.DocumentUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.component.CasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.util.CasUtil;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.io.IOExce
