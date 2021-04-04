7e6cddd0d241c6597a43984ccb8a29ff64b4a457
cinderellab <cinderela80@proton.me>
2021-04-04 18:28:02 +0000

    remove unused parameter

--- Textokit.Commons/src/main/java/com/textocat/textokit/commons/io/axml/AXML2XMI.java
+++ Textokit.Commons/src/main/java/com/textocat/textokit/commons/io/axml/AXML2XMI.java
@@ -27,4 +27,70 @@ import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.resource.m
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.CasCreationUtils;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+
+/**
+ * An application. Reads all {@code *.xml} files in the specified input
+ * directory by {@link AXMLReader} and serialize resulting CASes in XMI format
+ * into the specified output directory.
+ * <p/>
+ * If an output directory is not specified then it is assumed to be the same as
+ * the input directory.
+ *
+ * @author Rinat Gareev
+ */
+public class AXML2XMI {
+
+    @Parameter(names = {"-i", "--input-dir"}, required = true)
+    private File inputDir;
+    @Parameter(names = {"-ts"}, required = true,
+            description = "Typesystem description name. "
+                    + "UIMA mechanics assumes that it must be in the classpath or the UIMA data path")
+    private String typeSystemDescName;
+    @Parameter(names = {"-o", "--output-dir"}, required = false)
+    private File outputDir;
+
+    private AXML2XMI() {
+    }
+
+    public static void main(String[] args) throws UIMAException, IOException, SAXException {
+        AXML2XMI launcher = new AXML2XMI();
+        new JCommander(launcher, args);
+        if (launcher.outputDir == null) {
+            launcher.outputDir = launcher.inputDir;
+        }
+        launcher.run();
+    }
+
+    private void run() throws ResourceInitializationException, IOException, SAXException {
+        Collection<File> inputFiles =
+                FileUtils.listFiles(inputDir, FileFilterUtils.suffixFileFilter(".xml"), null);
+        if (inputFiles.isEmpty()) {
+            return;
+        }
+        TypeSystemDescription tsd = TypeSystemDescriptionFactory
+                .createTypeSystemDescription(typeSystemDescName);
+        CAS cas = CasCreationUtils.createCas(tsd, null, null);
+        for (File inputFile : inputFiles) {
+            AXMLReader.read(inputFile, cas);
+            File outFile = getOutputFile(inputFile);
+            OutputStream out = FileUtils.openOutputStream(outFile);
+            try {
+                XmiCasSerializer.serialize(cas, null, out, true, null);
+            } finally {
+                out.close();
+                cas.reset();
+            }
+        }
+    }
+
+    private File getOutputFile(File inputFile) {
+        String baseName = FilenameUtils.getBaseName(inputFile.getPath());
+     
