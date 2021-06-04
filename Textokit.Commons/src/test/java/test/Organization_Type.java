fc2bf69ac13d63c87e5f968b85f998a21776ef51
cinderellab <cinderela80@proton.me>
2021-06-04 07:45:23 +0000

    Clean up some warnings

--- Textokit.Commons/src/test/java/test/Organization_Type.java
+++ Textokit.Commons/src/test/java/test/Organization_Type.java
@@ -9,12 +9,14 @@ import org.apache.uima.cas.impl.FSGenerator;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
 /** 
  * Updated by JCasGen Thu Dec 24 18:21:01 MSK 2015
  * @generated */
-public class GPE_Type extends Annotation_Type {
+public class Organization_Type extends Annotation_Type {
   /** @generated 
    * @return the generator for this type
    */
@@ -24,40 +26,16 @@ public class GPE_Type extends Annotation_Type {
   private final FSGenerator fsGenerator = 
     new FSGenerator() {
       public FeatureStructure createFS(int addr, CASImpl cas) {
-  			 if (GPE_Type.this.useExistingInstance) {
+  			 if (Organization_Type.this.useExistingInstance) {
   			   // Return eq fs instance if already created
-  		     FeatureStructure fs = GPE_Type.this.jcas.getJfsFromCaddr(addr);
+  		     FeatureStructure fs = Organization_Type.this.jcas.getJfsFromCaddr(addr);
   		     if (null == fs) {
-  		       fs = new GPE(addr, GPE_Type.this);
-  			   GPE_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  		       fs = new Organization(addr, Organization_Type.this);
+  			   Organization_Type.this.jcas.putJfsFromCaddr(addr, fs);
   			   return fs;
   		     }
   		     return fs;
-        } else return new GPE(addr, GPE_Type.this);
+        } else return new Organization(addr, Organization_Type.this);
   	  }
     };
-  /** @generated */
-  @SuppressWarnings ("hiding")
-  public final static int typeIndexID = GPE.typeIndexID;
-  /** @generated 
-     @modifiable */
-  @SuppressWarnings ("hiding")
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("test.GPE");
-
-
-
-  /** initialize variables to correspond with Cas Type and Features
-	 * @generated
-	 * @param jcas JCas
-	 * @param casType Type 
-	 */
-  public GPE_Type(JCas jcas, Type casType) {
-    super(jcas, casType);
-    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
-
-  }
-}
-
-
-
-    
+  /
