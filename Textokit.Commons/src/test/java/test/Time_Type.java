6f5266685758d4d3d6438a1530c2ab27c456313d
cinderellab <cinderela80@proton.me>
2021-06-06 06:35:25 +0000

    ALSA: usb-audio: Fix TEAC UD-501/UD-503/NT-503 usb delay

--- Textokit.Commons/src/test/java/test/Time_Type.java
+++ Textokit.Commons/src/test/java/test/Time_Type.java
@@ -14,7 +14,7 @@ import org.apache.uima.jcas.tcas.Annotation_Type;
 /** 
  * Updated by JCasGen Thu Dec 24 18:21:01 MSK 2015
  * @generated */
-public class PersonName_Type extends Annotation_Type {
+public class Time_Type extends Annotation_Type {
   /** @generated 
    * @return the generator for this type
    */
@@ -24,40 +24,21 @@ public class PersonName_Type extends Annotation_Type {
   private final FSGenerator fsGenerator = 
     new FSGenerator() {
       public FeatureStructure createFS(int addr, CASImpl cas) {
-  			 if (PersonName_Type.this.useExistingInstance) {
+  			 if (Time_Type.this.useExistingInstance) {
   			   // Return eq fs instance if already created
-  		     FeatureStructure fs = PersonName_Type.this.jcas.getJfsFromCaddr(addr);
+  		     FeatureStructure fs = Time_Type.this.jcas.getJfsFromCaddr(addr);
   		     if (null == fs) {
-  		       fs = new PersonName(addr, PersonName_Type.this);
-  			   PersonName_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  		       fs = new Time(addr, Time_Type.this);
+  			   Time_Type.this.jcas.putJfsFromCaddr(addr, fs);
   			   return fs;
   		     }
   		     return fs;
-        } else return new PersonName(addr, PersonName_Type.this);
+        } else return new Time(addr, Time_Type.this);
   	  }
     };
   /** @generated */
   @SuppressWarnings ("hiding")
-  public final static int typeIndexID = PersonName.typeIndexID;
+  public final static int typeIndexID = Time.typeIndexID;
   /** @generated 
      @modifiable */
-  @SuppressWarnings ("hiding")
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("test.PersonName");
-
-
-
-  /** initialize variables to correspond with Cas Type and Features
-	 * @generated
-	 * @param jcas JCas
-	 * @param casType Type 
-	 */
-  public PersonName_Type(JCas jcas, Type casType) {
-    super(jcas, casType);
-    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
-
-  }
-}
-
-
-
-    
+  @SuppressWarnings ("h
