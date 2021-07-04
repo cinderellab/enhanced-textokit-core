68985ce4e2c7fe18020dd31f24b47c0e421a20b0
cinderellab <cinderela80@proton.me>
2021-07-04 11:32:34 +0000

    some small tweaks

--- Textokit.Corpus.Statistics/src/main/java/com/textocat/textokit/corpus/statistics/app/UnitsTSVKappaCalculator.java
+++ Textokit.Corpus.Statistics/src/main/java/com/textocat/textokit/corpus/statistics/app/UnitsTSVKappaCalculator.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright 2015 Textocat
  *
@@ -17,11 +18,12 @@
 package com.textocat.textokit.corpus.statistics.app;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import com.textocat.textokit.corpus.statistics.dao.units.InMemoryUnitsDAO;
 import com.textocat.textokit.corpus.statistics.dao.units.Unit;
 import com.textocat.textokit.corpus.statistics.dao.units.UnitsDAO;
 import de.tudarmstadt.ukp.dkpro.statistics.agreement.AnnotationStudy;
-import de.tudarmstadt.ukp.dkpro.statistics.agreement.MultiRaterPiAgreement;
+import de.tudarmstadt.ukp.dkpro.statistics.agreement.TwoRaterKappaAgreement;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,48 +33,52 @@ import java.io.IOException;
 import java.io.Reader;
 import java.net.URISyntaxException;
 
-public class UnitsTSVAgreementCalculator {
+public class UnitsTSVKappaCalculator {
 
-    final Logger logger = LoggerFactory
-            .getLogger(UnitsTSVAgreementCalculator.class);
+    private final Logger logger = LoggerFactory
+            .getLogger(UnitsTSVKappaCalculator.class);
+
+    @Parameter(names = "-tsv", description = "Input TSV file", required = true)
+    public String tsv;
 
     private UnitsDAO dao;
-    UnitsTSVAgreementCalculatorParams calculatorParams;
 
     public static void main(String[] args) throws IOException,
             URISyntaxException {
-        UnitsTSVAgreementCalculator calculator = new UnitsTSVAgreementCalculator(
-                args);
+        UnitsTSVKappaCalculator calculator = new UnitsTSVKappaCalculator(args);
         calculator.calculate();
     }
 
-    public UnitsTSVAgreementCalculator(String[] args) throws IOException,
+    public UnitsTSVKappaCalculator(String[] args) throws IOException,
             URISyntaxException {
-        calculatorParams = new UnitsTSVAgreementCalculatorParams();
-        new JCommander(calculatorParams, args);
+        new JCommander(this, args);
 
         dao = new InMemoryUnitsDAO();
-        Reader tsvIn = new FileReader(calculatorParams.tsv);
+        Reader tsvIn = new FileReader(tsv);
         dao.addUnitsFromTSV(tsvIn);
         IOUtils.closeQuietly(tsvIn);
 
     }
 
     public void calculate() {
-        AnnotationStudy study = new AnnotationStudy(
-                calculatorParams.annotatorCount);
+        AnnotationStudy study = new AnnotationStudy(2);
+        int units = 0;
+        int unitsDoneBySingleAnnotator = 0;
         for (Unit unit : dao.getUnits()) {
+            units++;
             String[] classes = unit.getSortedClasses();
-            if (classes.length == calculatorParams.annotatorCount) {
+            if (classes.length == 2) {
                 study.addItemAsArray(classes);
             } else {
-                logger.warn(
-                        "Unit {} has wrong annotators count, so it is not included in study.",
-                        unit);
+                unitsDoneBySingleAnnotator++;
             }
         }
-        MultiRaterPiAgreement pi = new MultiRaterPiAgreement(study);
-        System.out.println(pi.calculateAgreement());
+        logger.info("Read units: {}. There are {} units covered by an only annotator",
+                units, unitsDoneBySingleAnnotator);
+        TwoRaterKappaAgreement kappa = new TwoRaterKappaAgreement(study);
+        System.out.println(String.format("Kappa: %s\nObserved agr: %s\nExpected %s",
+                kappa.calculateAgreement(),
+                kappa.calculateObservedAgreement(), kappa.calculateExpectedAgreement()));
 
     }
 }
