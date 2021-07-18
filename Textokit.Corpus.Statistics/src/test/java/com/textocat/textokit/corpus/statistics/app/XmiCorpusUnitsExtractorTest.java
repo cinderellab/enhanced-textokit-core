76debda6fc8f41719543836d87e55df552a5176f
cinderellab <cinderela80@proton.me>
2021-07-18 07:36:57 +0000

    * Minor fixes

--- Textokit.Corpus.Statistics/src/test/java/com/textocat/textokit/corpus/statistics/app/XmiCorpusUnitsExtractorTest.java
+++ Textokit.Corpus.Statistics/src/test/java/com/textocat/textokit/corpus/statistics/app/XmiCorpusUnitsExtractorTest.java
@@ -10,4 +10,40 @@
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governin
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.corpus.statistics.app;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class XmiCorpusUnitsExtractorTest {
+
+    String corpusPathString = Thread.currentThread().getContextClassLoader()
+            .getResource("corpus_example").getPath();
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Test
+    public void test() throws IOException, SAXException,
+            CpeDescriptorException, ParserConfigurationException, UIMAException {
+        XmiCorpusUnitsExtractor.main(new String[]{"-corpus",
+                corpusPathString, "-unit",
+                "com.textocat.textokit.tokenizer.fstype.W", "-class",
+                "ru.kfu.itis.issst.evex.Person", "-class",
+                "ru.kfu.itis.issst.evex.Organization", "-class",
+                "ru.kfu.itis.issst.evex.Weapon", "-output",
+                tempFolder.newFile().getPath()});
+    }
+
+}
