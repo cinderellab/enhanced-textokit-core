65d90fd08ac6aa582e793bb3bb8af561c67dd98b
cinderellab <cinderela80@proton.me>
2021-08-20 16:07:24 +0000

    Regenerate gemspec for version 1.0.0

--- Textokit.DictionaryMatcher/src/main/java/com/textocat/textokit/dictmatcher/DictionaryAnnotator.java
+++ Textokit.DictionaryMatcher/src/main/java/com/textocat/textokit/dictmatcher/DictionaryAnnotator.java
@@ -23,4 +23,52 @@ import com.textocat.textokit.chunk.Chunker;
 import com.textocat.textokit.commons.cas.FSTypeUtils;
 import com.textocat.textokit.commons.cas.FSUtils;
 import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_engine.Ana
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.*;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.component.CasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.ExternalResource;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.initializable.InitializableFactory;
+import org.apache.uima.fit.util.CasUtil;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.List;
+import java.util.RandomAccess;
+import java.util.Set;
+
+/**
+ * @param <V> a type of dictionary metadata ({@link com.textocat.textokit.chunk.Chunk}) metadata.
+ * @author Rinat Gareev
+ */
+public class DictionaryAnnotator<V> extends CasAnnotator_ImplBase {
+
+    /**
+     * Creates a description with {@link DefaultChunkAnnotationAdapter} and other defaults.
+     *
+     * @param resultAnnoClass a type of annotation that will be created for each matched chunk
+     * @return a description instance
+     * @throws ResourceInitializationException
+     */
+    public static AnalysisEngineDescription createDescription(Class<? extends AnnotationFS> resultAnnoClass)
+            throws ResourceInitializationException {
+        return AnalysisEngineFactory.createEngineDescription(DictionaryAnnotator.class,
+                DefaultChunkAnnotationAdapter.PARAM_RESULT_ANNOTATION_TYPE, resultAnnoClass.getName());
+    }
+
+    public static final String RESOURCE_CHUNKER = "chunker";
+    public static final String PARAM_CHUNK_ADAPTER_CLASS = "chunkAdapterClass";
+    public static final String PARAM_INPUT_TOKEN_TYPE = "inputTokenType";
+    public static final String PARAM_NORM_FORM_FEATURE_PATH = "normFormFeaturePath";
+    public static final String PARAM_BOUNDARY_ANNOTATION_TYPE = "boundaryAnnoType";
+    public static final String PARAM_NORM_FALLBACK_TO_COVERED_TEXT = "normFallbackToCoveredText";
+
+    @ExternalResource(key = RESOURCE_CHUNKER)
+    private Chunker<V> dictMatcher;
+    @ConfigurationParameter(name = PARAM_CHUNK_ADAPTER_CLASS, mandatory = false,
+            defaultValue = "com.textocat.textokit.dictmatcher.DefaultChunkAnnotationAdapter")
+    private Class<? extends ChunkAnnotationAdapter> chunkAdapterClass;
+    @ConfigurationParameter(name = PARAM_INPUT_TOKEN_TYPE, mandatory = false,
+            defaultValue = "com.textocat.textokit.morph.fs.Simp
