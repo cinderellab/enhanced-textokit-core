1aa9045ce0996a4806e428ad3d14109ffd3f8627
cinderellab <cinderela80@proton.me>
2021-08-22 12:11:29 +0000

    Modify .travis.yml

--- Textokit.DictionaryMatcher/src/main/java/com/textocat/textokit/dictmatcher/NotCapDictMatchRemover.java
+++ Textokit.DictionaryMatcher/src/main/java/com/textocat/textokit/dictmatcher/NotCapDictMatchRemover.java
@@ -26,4 +26,68 @@ import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.fit.factory.AnalysisEngineFact
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.Set;
+
+import static java.lang.String.format;
+
+/**
+ * Deletes annotations that start with a token with a lower-case first letter.
+ *
+ * @author Rinat Gareev
+ */
+public class NotCapDictMatchRemover extends JCasAnnotator_ImplBase {
+
+    public static AnalysisEngineDescription createDescription(
+            Class<? extends DictionaryMatch> targetAnnotationClass) throws ResourceInitializationException {
+        return AnalysisEngineFactory.createEngineDescription(NotCapDictMatchRemover.class,
+                PARAM_TARGET_ANNOTATION_TYPE, targetAnnotationClass.getName());
+    }
+
+    public static final String PARAM_TARGET_ANNOTATION_TYPE = "targetAnnotationType";
+
+    @ConfigurationParameter(name = PARAM_TARGET_ANNOTATION_TYPE)
+    private Class<? extends DictionaryMatch> targetAnnotationClass;
+
+
+    @Override
+    public void process(JCas jCas) throws AnalysisEngineProcessException {
+        Set<DictionaryMatch> dm2Delete = Sets.newHashSet();
+        for (DictionaryMatch dm : JCasUtil.select(jCas, targetAnnotationClass)) {
+            Token firstToken = getFirstToken(dm);
+            if (firstToken instanceof SW) {
+                dm2Delete.add(dm);
+            }
+        }
+        for (DictionaryMatch dm : dm2Delete) {
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug(format(
+                        "Dictionary match '%s' is deleted by NotCap rule", dm.getCoveredText()));
+            }
+            dm.removeFromIndexes();
+        }
+    }
+
+    private static Token getFirstToken(DictionaryMatch dm) {
+        Annotation _firstToken = dm.getFirstToken();
+        if (_firstToken == null) {
+            throw new UnsupportedOperationException();
+        }
+        if (_firstToken instanceof Token) {
+            return (Token) _firstToken;
+        }
+        if (_firstToken instanceof SimplyWord) {
+            return ((SimplyWord) _firstToken).getToken();
+        }
+        if (_firstToken instanceof Word) {
+            return ((Word) _firstToken).getToken();
+        }
+        throw new UnsupportedOperationException(
+                format("Unknown token annotation type: %s", _firstToken.getType()));
+    }
+}
