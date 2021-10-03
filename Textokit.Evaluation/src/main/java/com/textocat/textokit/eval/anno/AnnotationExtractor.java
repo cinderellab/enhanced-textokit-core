915aaf3c9bbb30847057558cad3d9959aa1e32d4
cinderellab <cinderela80@proton.me>
2021-10-03 15:32:20 +0000

    Update History.md and AUTHORS for new release

--- Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/AnnotationExtractor.java
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/AnnotationExtractor.java
@@ -9,4 +9,23 @@
  *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDI
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.eval.anno;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.text.AnnotationFS;
+
+/**
+ * @author Rinat Gareev
+ */
+public interface AnnotationExtractor {
+
+    // TODO could we return some other non-UIMA class? Like java.util.Iterator
+    FSIterator<AnnotationFS> extract(CAS cas);
+
+}
---
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/DocumentMetaExtractor.java
@@ -0,0 +1,54 @@
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.eval.anno;
+
+import com.textocat.textokit.commons.cas.AnnotationUtils;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Rinat Gareev
+ */
+public class DocumentMetaExtractor {
+
+    @Autowired
+    private TypeSystem typeSystem;
+    @Value("${document.meta.annotationType}")
+    private String docUriAnnotationType;
+    @Value("${document.meta.uriFeatureName}")
+    private String docUriFeatureName;
+    // derived
+    private Type docMetaType;
+    private Feature docUriFeature;
+
+    @SuppressWarnings("unused")
+    @PostConstruct
+    private void init() {
+        docMetaType = typeSystem.getType(docUriAnnotationType);
+        if (docMetaType == null) {
+            throw new IllegalStateException("Can't find annotation type '"
+                    + docUriAnnotationType + "'");
+        }
+        docUriFeature = docMetaType.getFeatureByBaseName(docUriFeatureName);
+        if (docUriFeature == null) {
+            throw new Il
