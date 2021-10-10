148f2f70e33825048ed37ab39fdca81f293c821b
cinderellab <cinderela80@proton.me>
2021-10-10 12:45:52 +0000

    Update topics.

--- Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/impl/OverlapMatchingStrategy.java
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/impl/OverlapMatchingStrategy.java
@@ -19,3 +19,65 @@ package com.textocat.textokit.eval.anno.impl;
 
 import com.textocat.textokit.commons.cas.AnnotationUtils;
 import com.textocat.textokit.commons.cas.OverlapIndex;
+import com.textocat.textokit.eval.anno.AnnotationExtractor;
+import com.textocat.textokit.eval.anno.MatchingStrategy;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author Rinat Gareev
+ */
+/*
+ * TODO OverlapMatchingStrategy, AnnotationExtractor and TypeBasedMatcherDispatcher 
+ * seem to be highly-coupled concepts.
+ */
+public abstract class OverlapMatchingStrategy implements MatchingStrategy {
+
+    @Autowired
+    private AnnotationExtractor annotationExtractor;
+    // state fields
+    private CAS sysCas;
+    private OverlapIndex<AnnotationFS> sysOverlapIdx;
+
+    @Override
+    public void changeCas(CAS newSysCas) {
+        sysCas = newSysCas;
+        // reset state related to previous CAS
+        sysOverlapIdx = null;
+        if (sysCas != null) {
+            sysOverlapIdx = AnnotationUtils.createOverlapIndex(annotationExtractor.extract(sysCas));
+        }
+    }
+
+    @Override
+    public Set<AnnotationFS> searchCandidates(AnnotationFS goldAnno) {
+        Set<AnnotationFS> result = sysOverlapIdx.getOverlapping(
+                goldAnno.getBegin(),
+                goldAnno.getEnd());
+        Iterator<AnnotationFS> resultIter = result.iterator();
+        while (resultIter.hasNext()) {
+            if (!isCandidate(goldAnno, resultIter.next())) {
+                resultIter.remove();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public AnnotationFS searchExactMatch(AnnotationFS goldAnno, Iterable<AnnotationFS> candidates) {
+        for (AnnotationFS candAnno : candidates) {
+            if (match(goldAnno, candAnno)) {
+                return candAnno;
+            }
+        }
+        return null;
+    }
+
+    protected abstract boolean match(AnnotationFS goldAnno, AnnotationFS candAnno);
+
+    protected abstract boolean isCandidate(AnnotationFS goldAnno, AnnotationFS sysAnno);
+}
---
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/anno/impl/TypeBasedExtractor.java
@@ -0,0 +1,73 @@
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.eval.anno.impl;
+
+import com.textocat.textokit.eval.ConfigurationKeys;
+import com.textocat.textokit.eval.anno.AnnotationExtractor;
+import org.apache.uima.cas.*;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.apache.commons.lang3.StringUtils.split;
+
+/**
+ * @author Rinat Gareev
+ */
+public class TypeBasedExtractor implements AnnotationExtractor {
+
+    @Autowired
+    private TypeSystem typeSystem;
+
+    @Value("${" + ConfigurationKeys.KEY_ANNOTATION_TYPES + "}")
+    private String annoTypeNamesString;
+
+    // derived
+    private Set<Type> annoTypes;
+    private FSMatchConstraint annoMatchConstraint;
+
+    @SuppressWarnings("unused")
+    @PostConstruct
+    private void init() {
+        Set<String> annoTypeNames = newHashSet(split(annoTypeNamesString, " ,;"));
+        annoTypes = new HashSet<Type>();
+        for (String curTypeName : annoTypeNames) {
+            Type curType = typeSystem.getType(curTypeName);
+            if (curType == null) {
+                throw new IllegalStateException("Can't find type " + curTypeName);
+            }
+            annoTypes.add(curType);
+        }
+        // prepare FS constraint
+        ConstraintFactory cf = ConstraintFactory.instance();
+        FSTypeConstraint typeConstr = cf.createTypeConstraint();
+        for (Type curType : annoTypes) {
+            typeConstr.add(curType);
+        }
+        annoMatchConstraint = typeConstr;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public FSIterator<Annota
