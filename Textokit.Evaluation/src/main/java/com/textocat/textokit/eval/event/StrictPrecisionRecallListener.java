5cca4daacc056fd7e937aeaf874fe3a96c9db7f9
cinderellab <cinderela80@proton.me>
2021-10-30 05:57:11 +0000

    improve log messages

--- Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/event/StrictPrecisionRecallListener.java
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/event/StrictPrecisionRecallListener.java
@@ -9,4 +9,87 @@
  *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
- 
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.eval.event;
+
+import com.textocat.textokit.eval.measure.RecognitionMeasures;
+import org.apache.uima.cas.text.AnnotationFS;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Rinat Gareev
+ */
+public class StrictPrecisionRecallListener extends TypedPrintingEvaluationListener {
+
+    // state fields
+    private RecognitionMeasures measures = new RecognitionMeasures();
+
+    @Override
+    @PostConstruct
+    protected void init() throws Exception {
+        super.init();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onMissing(AnnotationFS goldAnno) {
+        if (!checkType(goldAnno)) {
+            return;
+        }
+        measures.incrementMissing(1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onExactMatch(AnnotationFS goldAnno, AnnotationFS sysAnno) {
+        if (!checkType(goldAnno)) {
+            return;
+        }
+        measures.incrementMatching(1);
+    }
+
+    @Override
+    public void onPartialMatch(AnnotationFS goldAnno, AnnotationFS sysAnno) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onSpurious(AnnotationFS sysAnno) {
+        if (!checkType(sysAnno)) {
+            return;
+        }
+        measures.incrementSpurious(1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onEvaluationComplete() {
+        String report = MeasuresReportUtils.getReportString(targetType, measures);
+        printer.println(report);
+        clean();
+    }
+
+    public RecognitionMeasures getMeasures() {
+        return measures;
+    }
+
+    private boolean checkType(AnnotationFS anno) {
+        if (targetType == null) {
+            return true;
+        }
+        return ts.subsumes(targetType, anno.getType());
+    }
+}
