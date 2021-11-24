6e6eacf290f717551067b6a57bebbcc9eec09940
cinderellab <cinderela80@proton.me>
2021-11-24 05:25:00 +0000

    cpufreq: Remove unnecessary initialization of a local variable

--- Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/measure/RecognitionMeasures.java
+++ Textokit.Evaluation/src/main/java/com/textocat/textokit/eval/measure/RecognitionMeasures.java
@@ -31,3 +31,47 @@ public class RecognitionMeasures {
 
     public void incrementMissing(float delta) {
         missingCounter += delta;
+    }
+
+    public void incrementSpurious(float delta) {
+        spuriousCounter += delta;
+    }
+
+    public void incrementMatching(float delta) {
+        matchingCounter += delta;
+    }
+
+    public float getPrecision() {
+        float recognizedScore = matchingCounter + spuriousCounter;
+        return matchingCounter / recognizedScore;
+    }
+
+    public float getRecall() {
+        return matchingCounter / (matchingCounter + missingCounter);
+    }
+
+    public float getF1() {
+        float precision = getPrecision();
+        float recall = getRecall();
+        return 2 * precision * recall / (precision + recall);
+    }
+
+    public float getMatchedScore() {
+        return matchingCounter;
+    }
+
+    public float getSpuriousScore() {
+        return spuriousCounter;
+    }
+
+    public float getMissedScore() {
+        return missingCounter;
+    }
+
+    /**
+     * @return matchingScore + missingScore
+     */
+    public float getGoldScore() {
+        return matchingCounter + missingCounter;
+    }
+}
