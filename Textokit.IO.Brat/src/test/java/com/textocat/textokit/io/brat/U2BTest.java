fcccb0ca6b80487fad59f2e3302cc2aa7d068d12
cinderellab <cinderela80@proton.me>
2022-01-11 14:17:58 +0000

    libptytty sync

--- Textokit.IO.Brat/src/test/java/com/textocat/textokit/io/brat/U2BTest.java
+++ Textokit.IO.Brat/src/test/java/com/textocat/textokit/io/brat/U2BTest.java
@@ -7,4 +7,45 @@
  *
  *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.io.brat;
+
+import com.textocat.textokit.commons.cpe.XmiCollectionReader;
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.XMLInputSource;
+import org.junit.Test;
+
+public class U2BTest {
+
+    private static final String inputFileXMIDir = "data/news.xmi";
+    public static final String U2BAggregateDesc = "desc/aggregates/u2b-test-aggregate.xml";
+
+    @Test
+    public void test() throws Exception {
+        TypeSystemDescription tsd = TypeSystemDescriptionFactory
+                .createTypeSystemDescription("desc.types.test-TypeSystem");
+
+        CollectionReaderDescription colReaderDesc = CollectionReaderFactory.createReaderDescription(
+                XmiCollectionReader.class, tsd,
+                XmiCollectionReader.PARAM_INPUTDIR, inputFileXMIDir);
+
+        // configure AE
+        XMLInputSource aeDescInput = new XMLInputSource(U2BAggregateDesc);
+        AnalysisEngineDescription aeDesc = UIMAFramework.getXMLParser()
+                .parseAnalysisEngineDescription(aeDescInput);
+
+        SimplePipeline.runPipeline(colReaderDesc, aeDesc);
+    }
+}
