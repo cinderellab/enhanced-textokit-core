2fe64a9891a1894ae53dec0038e35def28dac598
cinderellab <cinderela80@proton.me>
2022-01-12 06:07:33 +0000

    Features

--- Textokit.IO.Brat/src/test/java/com/textocat/textokit/io/brat/UtilTest.java
+++ Textokit.IO.Brat/src/test/java/com/textocat/textokit/io/brat/UtilTest.java
@@ -15,4 +15,24 @@
  *    limitations under the License.
  */
 
-package com.te
+package com.textocat.textokit.io.brat;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Rinat Gareev
+ */
+public class UtilTest {
+
+    @Test
+    public void testBratToJavaNameConversions() {
+        Assert.assertEquals("TransferOwnership", PUtils.toProperJavaName("Transfer-ownership"));
+        Assert.assertEquals("TransferOwnership", PUtils.toProperJavaName("Transfer--ownership"));
+        Assert.assertEquals("TransferOwnership", PUtils.toProperJavaName("Transfer-Ownership"));
+        Assert.assertEquals("TransferOwnership", PUtils.toProperJavaName("Transfer--Ownership"));
+        Assert.assertEquals("Ownership", PUtils.toProperJavaName("-Ownership"));
+        Assert.assertEquals("Ownership", PUtils.toProperJavaName("--ownership"));
+    }
+
+}
---
+++ Textokit.IO.Brat/src/test/java/com/textocat/textokit/io/brat/test/CityNoteMapper.java
@@ -0,0 +1,45 @@
+
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.io.brat.test;
+
+import com.textocat.textokit.io.brat.BratNoteMapper;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.text.AnnotationFS;
+
+import static com.textocat.textokit.commons.util.AnnotatorUtils.annotationTypeExist;
+import static com.textocat.textokit.commons.util.AnnotatorUtils.featureExist;
+
+/**
+ * @author Rinat Gareev
+ */
+public class CityNoteMapper implements BratNoteMapper {
+
+    private static final String CITY_TYPE_NAME = "issst.test.HL_City";
+
+    private Feature latFeat;
+    private Feature longFeat;
+
+    @Override
+    public void typeSystemInit(TypeSystem ts) throws AnalysisEngineProcessException {
+        Type cityType = ts.getType(CITY_TYPE_NAME);
+        annotationTypeExist(CITY_TYPE_NAME, cityType);
+        longFeat = featureExist(cityType, "longitude");
+        latFeat = featureExist(cityType, "latitude");
