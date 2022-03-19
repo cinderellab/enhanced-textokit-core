98aad2f57109bde0a1ecb2849787c238fb7f1e99
cinderellab <cinderela80@proton.me>
2022-03-19 20:30:29 +0000

    Build scripts

--- Textokit.Morph.Dictionary.OpenCorpora/src/main/java/com/textocat/textokit/morph/opencorpora/resource/XmlDictionaryParser.java
+++ Textokit.Morph.Dictionary.OpenCorpora/src/main/java/com/textocat/textokit/morph/opencorpora/resource/XmlDictionaryParser.java
@@ -37,4 +37,66 @@ import static java.lang.System.currentTimeMillis;
  */
 public class XmlDictionaryParser {
 
-    pr
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private MorphDictionaryImpl dict;
+    private DictionaryExtension ext;
+    private InputStream in;
+
+    public XmlDictionaryParser(MorphDictionaryImpl dict, DictionaryExtension ext, InputStream in) {
+        this.dict = dict;
+        this.ext = ext;
+        this.in = in;
+    }
+
+    public void run() throws SAXException, IOException {
+        SAXParser xmlParser;
+        try {
+            xmlParser = SAXParserFactory.newInstance().newSAXParser();
+        } catch (ParserConfigurationException e) {
+            // should never happen
+            throw new IllegalStateException(e);
+        }
+        XMLReader xmlReader = xmlParser.getXMLReader();
+
+        DictionaryXmlHandler dictHandler = new DictionaryXmlHandler(dict);
+        if (ext.getLexemePostprocessors() != null) {
+            for (LemmaPostProcessor lpp : ext.getLexemePostprocessors()) {
+                dictHandler.addLemmaPostProcessor(lpp);
+            }
+        }
+        if (ext.getGramModelPostProcessors() != null) {
+            for (GramModelPostProcessor gmpp : ext.getGramModelPostProcessors()) {
+                dictHandler.addGramModelPostProcessor(gmpp);
+            }
+        }
+
+        xmlReader.setContentHandler(dictHandler);
+        InputSource xmlSource = new InputSource(in);
+        log.info("About to parse xml dictionary file");
+        long timeBefore = currentTimeMillis();
+        xmlReader.parse(xmlSource);
+        log.info("Parsing finished in {} ms", currentTimeMillis() - timeBefore);
+    }
+
+    public static MorphDictionaryImpl parse(InputStream in,
+                                            final LemmaPostProcessor... lemmaPostProcessors)
+            throws IOException, SAXException {
+        return parse(in, new DictionaryExtensionBase() {
+
+            @Override
+            public List<LemmaPostProcessor> getLexemePostprocessors() {
+                return Arrays.asList(lemmaPostProcessors);
+            }
+        });
+    }
+
+    public static MorphDictionaryImpl parse(InputStream in, DictionaryExtension ext)
+            throws IOException, SAXException {
+        MorphDictionaryImpl dict = new MorphDictionaryImpl();
+        parse(dict, in, ext);
+        return dict;
+    }
+
+    public static void parse(MorphDictionaryImpl dict, InputStream in, DictionaryExtension ext)
+        
