a4310cb8a0142c99c7b52c7542d2b575ed4bfe4c
cinderellab <cinderela80@proton.me>
2022-04-18 15:27:33 +0000

    Fixed potential NPE

--- Textokit.PhraseRecognizer.SPC/src/main/scala/com/textocat/textokit/phrrecog/parsing/WordUtils.scala
+++ Textokit.PhraseRecognizer.SPC/src/main/scala/com/textocat/textokit/phrrecog/parsing/WordUtils.scala
@@ -35,4 +35,35 @@ object WordUtils {
     require(w != null, "word annotation is null")
     if (w.getWordforms() == null) false
     else FSCollectionFactory.create(w.getWordforms(), classOf[Wordform]).exists { wf =>
-      val
+      val gramSet = toSet(wf.getGrammems())
+      // TODO more robust check for Part-of-Speech
+      gramSet.contains(pos) && grms.forall {
+        case GrammemeRequired(gr) => gramSet.contains(gr)
+        case GrammemeProhibited(gr) => !gramSet.contains(gr)
+      }
+    }
+  }
+
+  def findWordform(w: Word, pos: String, grms: GrammemeMatcher*): Option[Wordform] = {
+    require(w != null, "word annotation is null")
+    if (w.getWordforms() == null) None
+    else FSCollectionFactory.create(w.getWordforms(), classOf[Wordform]).find { wf =>
+      val gramSet = toSet(wf.getGrammems())
+      // TODO more robust check for Part-of-Speech
+      gramSet.contains(pos) && grms.forall {
+        case GrammemeRequired(gr) => gramSet.contains(gr)
+        case GrammemeProhibited(gr) => !gramSet.contains(gr)
+      }
+    }
+  }
+
+  def has(gr: String) = new GrammemeRequired(gr)
+
+  def hasNot(gr: String) = new GrammemeProhibited(gr)
+}
+
+sealed abstract class GrammemeMatcher
+
+case class GrammemeRequired(gr: String) extends GrammemeMatcher
+
+case class GrammemeProhibited(gr: String) extends GrammemeMatcher
