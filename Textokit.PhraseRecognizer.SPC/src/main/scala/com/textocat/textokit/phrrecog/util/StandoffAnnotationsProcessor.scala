d7b6a2e9e3b3d11033fa4fea24404e6d42e536d1
cinderellab <cinderela80@proton.me>
2022-04-19 14:13:56 +0000

    Subpage mod.

--- Textokit.PhraseRecognizer.SPC/src/main/scala/com/textocat/textokit/phrrecog/util/StandoffAnnotationsProcessor.scala
+++ Textokit.PhraseRecognizer.SPC/src/main/scala/com/textocat/textokit/phrrecog/util/StandoffAnnotationsProcessor.scala
@@ -52,4 +52,50 @@ class StandoffAnnotationsProcessor extends JCasAnnotator_ImplBase {
   override def initialize(ctx: UimaContext) {
     super.initialize(ctx)
     val annoStrParserFactoryClassName = ctx.getConfigParameterValue(ParamAnnotationStringParserFactoryClass).asInstanceOf[String]
-    mandatoryParam(ParamAnnotationStringParserFactoryClass, annoStrParserFactoryCla
+    mandatoryParam(ParamAnnotationStringParserFactoryClass, annoStrParserFactoryClassName)
+    val annoStrParserClass = Class.forName(annoStrParserFactoryClassName)
+    annoStrParserFactory = annoStrParserClass.newInstance().asInstanceOf[PhraseStringParsersFactory]
+  }
+
+  override def process(jCas: JCas) {
+    val annFile = getAnnFile(jCas)
+    val annLines = Source.fromFile(annFile, "utf-8").getLines()
+    val paraAnnotations = JCasUtil.select(jCas, classOf[Paragraph])
+    tokenType = jCas.getCasType(Token.typeIndexID)
+    for ((paragraph, paragrNum) <- paraAnnotations.zipWithIndex)
+      if (annLines.hasNext)
+        parseAnnoLine(paragraph, annLines.next())
+      else throw new IllegalStateException("Standoff annotations files ends before line %s".format(paragrNum))
+  }
+
+  private def parseAnnoLine(paraAnno: AnnotationFS, annLine: String) {
+    val jCas = paraAnno.getCAS().getJCas()
+    val paraOffset = paraAnno.getBegin
+    val phraseStrings = annLine.split("\\|")
+    val paraTokens = getParagraphTokens(paraAnno)
+    val annoStrParser = annoStrParserFactory.createParser(jCas, paraTokens)
+    for (phrStr <- phraseStrings)
+      annoStrParser.parse(phrStr).addToIndexes()
+  }
+
+  private def getParagraphTokens(paraAnno: AnnotationFS): Array[AnnotationFS] = {
+    val tokensList = CasUtil.selectCovered(paraAnno.getCAS(), tokenType, paraAnno)
+    tokensList.toArray(new Array[AnnotationFS](tokensList.size()))
+  }
+
+  private def getAnnFile(jCas: JCas): File = {
+    val docMeta = AnnotationUtils.getSingleAnnotation(jCas, classOf[DocumentMetadata])
+    if (docMeta == null) throw new IllegalStateException("No DocumentMetadata")
+    val docUri = new URI(docMeta.getSourceUri())
+    val docFile = new File(docUri)
+    val baseName = FilenameUtils.getBaseName(docFile.getName())
+    val annFile = new File(docFile.getParent(), baseName + ".ann")
+    if (!annFile.isFile())
+      throw new IllegalStateException("No ann file for %s".format(docFile))
+    annFile
+  }
+}
+
+object StandoffAnnotationsProcessor {
+  val ParamAnnotationStringParserFactoryClass = "AnnotationStringParserFactoryClass"
+}
