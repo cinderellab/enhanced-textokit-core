2a60a6aefca602425c7f35383b8596e6c99ae46b
cinderellab <cinderela80@proton.me>
2022-04-29 11:24:10 +0000

    Version 1.0.9

--- Textokit.PosTagger.API/src/main/java/com/textocat/textokit/ml/NGCAgreementFeatureExtractor.java
+++ Textokit.PosTagger.API/src/main/java/com/textocat/textokit/ml/NGCAgreementFeatureExtractor.java
@@ -59,4 +59,42 @@ public class NGCAgreementFeatureExtractor implements FeatureExtractor1 {
     @Override
     public List<Feature> extract(JCas view, Annotation focusAnnotation)
             throws CleartkExtractorException {
-     
+        Word focusWord = PUtils.getWordAnno(view, focusAnnotation);
+        if (focusWord == null || focusWord.getWordforms() == null
+                || focusWord.getWordforms().size() == 0) {
+            return ImmutableList.of();
+        }
+        Word precWord = getPrecedingWord(view, focusWord);
+        if (precWord == null || precWord.getWordforms() == null
+                || precWord.getWordforms().size() == 0) {
+            return ImmutableList.of();
+        }
+        Wordform curWf = (Wordform) focusWord.getWordforms().get(0);
+        Wordform precWf = (Wordform) precWord.getWordforms().get(0);
+        if (precWf == null) {
+            return ImmutableList.of();
+        }
+        // to Bitset
+        BitSet curTag = MorphCasUtils.toGramBitSet(gramModel, curWf);
+        BitSet precTag = MorphCasUtils.toGramBitSet(gramModel, precWf);
+        // generate features
+        List<Feature> result = Lists.newLinkedList();
+        for (Map.Entry<String, TwoTagPredicate> npEntry : namedPredicates.entrySet()) {
+            TwoTagPredicate predicate = npEntry.getValue();
+            String predicateName = npEntry.getKey();
+            if (predicate.apply(precTag, curTag)) {
+                result.add(new Feature(predicateName, true));
+            }
+        }
+        return result;
+    }
+
+    private Word getPrecedingWord(JCas jCas, Word curWord) {
+        List<Word> precedingWords = JCasUtil.selectPreceding(jCas, Word.class, curWord, 1);
+        if (!precedingWords.isEmpty()) {
+            return precedingWords.get(0);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Textokit.PosTagger.API/src/main/java/com/textocat/textokit/ml/PUtils.java
@@ -0,0 +1,52 @@
+
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.ml;
+
+import com.textocat.textokit.morph.fs.Word;
+import com.textocat.textokit.tokenizer.fstype.Token;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.cleartk.ml.feature.extractor.CleartkExtractorException;
+
+import java.util.List;
+
+/**
+ * @author Rinat Gareev
+ */
+class PUtils {
+
+    public static Word getWordAnno(JCas view, Annotation focusAnnotation)
+            throws CleartkExtractorException {
+        Word wordAnno = null;
+        if (focusAnnotation instanceof Word) {
+            wordAnno = (Word) focusAnnotation;
+        } else if (focusAnnotation instanceof Token) {
+            List<Word> wordsCovered = JCasUtil.selectCovered(Word.class, focusAnnotation);
+            if (!wordsCovered.isEmpty()) {
+                wordAnno = wordsCovered.get(0);
+            }
+        } else {
+            throw CleartkExtractorException.wrongAnnotationType(Word.class, focusAnnotation);
+        }
+        return wordAnno;
+    }
+
+    private PUtils() {
+    }
+}
