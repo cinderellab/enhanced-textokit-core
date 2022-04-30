d7894d48f75712fbd79cbf6bf839825da870fabf
cinderellab <cinderela80@proton.me>
2022-04-30 12:03:45 +0000

    adding versioning data

---
+++ Textokit.PosTagger.API/src/main/java/com/textocat/textokit/ml/SuffixFeatureExtractor.java
@@ -0,0 +1,75 @@
+
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.ml;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.textocat.textokit.tokenizer.fstype.Token;
+import com.textocat.textokit.tokenizer.fstype.W;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.cleartk.ml.Feature;
+import org.cleartk.ml.feature.extractor.CleartkExtractorException;
+import org.cleartk.ml.feature.extractor.FeatureExtractor1;
+
+import java.util.List;
+
+/**
+ * @author Rinat Gareev
+ */
+public class SuffixFeatureExtractor implements FeatureExtractor1 {
+
+    private int maxSuffixLength;
+
+    public SuffixFeatureExtractor(int maxSuffixLength) {
+        if (maxSuffixLength <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.maxSuffixLength = maxSuffixLength;
+    }
+
+    @Override
+    public List<Feature> extract(JCas view, Annotation focusAnnotation)
+            throws CleartkExtractorException {
+        if (focusAnnotation instanceof Token) {
+            if (!(focusAnnotation instanceof W)) {
+                return ImmutableList.of();
+            }
+        }
+        String str = focusAnnotation.getCoveredText();
+        List<Feature> result = Lists.newLinkedList();
+        for (int suffixLength = 1; suffixLength <= maxSuffixLength; suffixLength++) {
+            String val;
+            if (str.length() <= suffixLength) {
+                val = str;
+            } else {
+                val = "*" + str.substring(str.length() - suffixLength);
+            }
+            result.add(new Feature(getFeatureName(suffixLength), val));
+            if (str.length() <= suffixLength) {
+                // suffix length increasing so there is no point to produce other features with different name
+                break;
+            }
+        }
+        return result;
+    }
+
+    private String getFeatureName(int suffixLength) {
+        return Feature.createName("Suffix", String.valueOf(suffixLength));
+    }
+}
---
+++ Textokit.PosTagger.API/src/main/java/com/textocat/textokit/ml/WordAnnotator.java
@@ -0,0 +1,66 @@
+
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.ml;
+
+import com.textocat.textokit.commons.cas.FSUtils;
+import com.textocat.textokit.morph.fs.Word;
+import com.textocat.textokit.morph.fs.Wordform;
+import com.textocat.textokit.segmentation.fstype.Sentence;
+import com.textocat.textokit.tokenizer.fstype.NUM;
+import com.textocat.textokit.tokenizer.fstype.Token;
+import com.textocat.textokit.tokenizer.fstype.W;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+/**
+ * Auxiliary annotator that makes Word annotations for each token that can carry
+ * them.
+ *
+ * @author Rinat Gareev
+ */
+public class WordAnnotator extends JCasAnnotator_ImplBase {
+
+    @Override
+    public void process(JCas jCas) throws AnalysisEngineProcessException {
+        makeWords(jCas);
+    }
+
+    public static void makeWords(JCas jCas) {
+        for (Sentence sent : JCasUtil.select(jCas, Sentence.class)) {
+            for (Token tok : JCasUtil.selectCovered(jCas, Token.class, sent)) {
+                if (canCarryWord(tok)) {
+                    Word word = new Word(jCas);
+                    word.setBegin(tok.getBegin());
+                    word.setEnd(tok.getEnd());
+                    word.setToken(tok);
+
+                    Wordform wf = new Wordform(jCas);
+                    wf.setWord(word);
+                    word.setWordforms(FSUtils.toFSArray(jCas, wf));
+                    word.addToIndexes();
+                }
+            }
+        }
+    }
+
+    public static boolean canCarryWord(Token tok) {
+        return tok instanceof W || tok instanceof NUM;
+    }
+}
