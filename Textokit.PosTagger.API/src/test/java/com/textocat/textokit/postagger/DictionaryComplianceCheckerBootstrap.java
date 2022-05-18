2ee3f4c738ad5977e56c126d14189b043701fa4f
cinderellab <cinderela80@proton.me>
2022-05-18 13:50:34 +0000

    Clean unused code

--- Textokit.PosTagger.API/src/test/java/com/textocat/textokit/postagger/DictionaryComplianceCheckerBootstrap.java
+++ Textokit.PosTagger.API/src/test/java/com/textocat/textokit/postagger/DictionaryComplianceCheckerBootstrap.java
@@ -19,4 +19,64 @@ package com.textocat.textokit.postagger;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.textocat.textokit.commons.cpe.XmiCollectionReader
+import com.textocat.textokit.commons.cpe.XmiCollectionReader;
+import com.textocat.textokit.commons.util.Slf4jLoggerImpl;
+import com.textocat.textokit.segmentation.SentenceSplitterAPI;
+import com.textocat.textokit.tokenizer.TokenizerAPI;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.ExternalResourceFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ExternalResourceDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+
+import java.io.File;
+import java.util.List;
+
+import static com.textocat.textokit.morph.dictionary.MorphDictionaryAPIFactory.getMorphDictionaryAPI;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+/**
+ * @author Rinat Gareev
+ */
+@Parameters(separators = " =")
+public class DictionaryComplianceCheckerBootstrap {
+
+    public static void main(String[] args) throws Exception {
+        DictionaryComplianceCheckerBootstrap instance = new DictionaryComplianceCheckerBootstrap();
+        new JCommander(instance, args);
+        // setup logging
+        Slf4jLoggerImpl.forceUsingThisImplementation();
+        instance.run();
+    }
+
+    @Parameter(names = {"-c", "--corpus-dir"}, required = true)
+    private File corpusDir;
+    @Parameter(names = {"-o", "--output-file"}, required = true)
+    private File outFile;
+    @Parameter(names = {"-p", "--pos-categories"}, required = true)
+    private List<String> posCategories;
+
+    private DictionaryComplianceCheckerBootstrap() {
+    }
+
+    private void run() throws Exception {
+        //
+        CollectionReaderDescription colReaderDesc;
+        {
+            TypeSystemDescription tsDesc = TypeSystemDescriptionFactory
+                    .createTypeSystemDescription(
+                            "com.textocat.textokit.commons.Commons-TypeSystem",
+                            TokenizerAPI.TYPESYSTEM_TOKENIZER,
+                            SentenceSplitterAPI.TYPESYSTEM_SENTENCES,
+                            PosTaggerAPI.TYPESYSTEM_POSTAGGER);
+            //
+            colReaderDesc = CollectionReaderFactory.createReaderDescription(
+                    XmiCollectionReader.class,
+                    tsDesc,
+                    XmiCollectionReader.PARAM_INPUTDIR, corpusDir.getPath());
+        }
+        //
+        Analys
