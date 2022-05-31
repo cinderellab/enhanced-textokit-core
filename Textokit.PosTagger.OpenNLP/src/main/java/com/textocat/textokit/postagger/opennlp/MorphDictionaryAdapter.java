a3e6d5eda80bd4ff1ec6a80ea440c2d624bb7e39
cinderellab <cinderela80@proton.me>
2022-05-31 06:07:02 +0000

    update translation template

--- Textokit.PosTagger.OpenNLP/src/main/java/com/textocat/textokit/postagger/opennlp/MorphDictionaryAdapter.java
+++ Textokit.PosTagger.OpenNLP/src/main/java/com/textocat/textokit/postagger/opennlp/MorphDictionaryAdapter.java
@@ -21,4 +21,35 @@ import com.textocat.textokit.morph.dictionary.WordUtils;
 import opennlp.tools.postag.TagDictionary;
 import org.apache.uima.UimaContext;
 import org.apache.uima.fit.component.initialize.ConfigurationParameterInitializer;
-import org.apache.uima.fit.descrip
+import org.apache.uima.fit.descriptor.ExternalResource;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.Set;
+
+/**
+ * @author Rinat Gareev
+ */
+public class MorphDictionaryAdapter implements TagDictionary {
+
+    public static final String RESOURCE_MORPH_DICTIONARY = "morphDict";
+    // XXX
+    static final String PARAM_GRAM_CATEGORIES = "gram.categories";
+
+    @ExternalResource()
+    private Set<String> gramCategories;
+
+    public MorphDictionaryAdapter(UimaContext ctx) throws ResourceInitializationException {
+        ConfigurationParameterInitializer.initialize(ctx, ctx);
+    }
+
+    @Override
+    public String[] getTags(String word) {
+        word = WordUtils.normalizeToDictionaryForm(word);
+        // XXX
+        throw new UnsupportedOperationException();
+    }
+
+    public Set<String> getGramCategories() {
+        return ImmutableSet.copyOf(gramCategories);
+    }
+}
