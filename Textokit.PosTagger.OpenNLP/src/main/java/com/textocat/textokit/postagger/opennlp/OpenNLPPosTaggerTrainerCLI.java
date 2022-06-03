ab02d3b34b4a08f91e969abb22479cc76d331414
cinderellab <cinderela80@proton.me>
2022-06-03 14:04:33 +0000

    Fix #39

--- Textokit.PosTagger.OpenNLP/src/main/java/com/textocat/textokit/postagger/opennlp/OpenNLPPosTaggerTrainerCLI.java
+++ Textokit.PosTagger.OpenNLP/src/main/java/com/textocat/textokit/postagger/opennlp/OpenNLPPosTaggerTrainerCLI.java
@@ -21,4 +21,61 @@ import com.beust.jcommander.Parameter;
 import com.textocat.textokit.commons.cpe.AnnotationIteratorOverCollection;
 import com.textocat.textokit.commons.cpe.XmiCollectionReader;
 import com.textocat.textokit.commons.util.Slf4jLoggerImpl;
-import com.texto
+import com.textocat.textokit.morph.commons.GramModelBasedTagMapper;
+import com.textocat.textokit.morph.commons.TagAssembler;
+import com.textocat.textokit.morph.dictionary.resource.MorphDictionary;
+import com.textocat.textokit.postagger.PosTaggerAPI;
+import com.textocat.textokit.postagger.PosTrimmingAnnotator;
+import com.textocat.textokit.segmentation.SentenceSplitterAPI;
+import com.textocat.textokit.segmentation.fstype.Sentence;
+import com.textocat.textokit.tokenizer.TokenizerAPI;
+import opennlp.tools.util.TrainingParameters;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ExternalResourceDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import static com.textocat.textokit.morph.dictionary.MorphDictionaryAPIFactory.getMorphDictionaryAPI;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.ExternalResourceFactory.bindExternalResource;
+import static org.apache.uima.fit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
+
+/**
+ * @author Rinat Gareev
+ */
+public class OpenNLPPosTaggerTrainerCLI {
+
+    static {
+        Slf4jLoggerImpl.forceUsingThisImplementation();
+    }
+
+    public static void main(String[] args) throws Exception {
+        OpenNLPPosTaggerTrainerCLI cli = new OpenNLPPosTaggerTrainerCLI();
+        new JCommander(cli, args);
+        //
+        OpenNLPPosTaggerTrainer trainer = new OpenNLPPosTaggerTrainer();
+        trainer.setLanguageCode(cli.languageCode);
+        trainer.setModelOutFile(cli.modelOutFile);
+        // train params
+        {
+            FileInputStream fis = FileUtils.openInputStream(cli.trainParamsFile);
+            TrainingParameters trainParams;
+            try {
+                trainParams = new TrainingParameters(fis);
+            } finally {
+                IOUtils.closeQuietly(fis);
+            }
+            trainer.setTrainingParameters(trainParams);
+        }
+        // feature extractors
+        {
+            FileInputStream fis = FileUtils
