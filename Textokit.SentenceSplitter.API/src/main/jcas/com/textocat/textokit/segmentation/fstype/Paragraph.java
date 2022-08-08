64a0b173527bf60749e6b51f4618121e6726036b
cinderellab <cinderela80@proton.me>
2022-08-08 10:25:59 +0000

    Updates Project

--- Textokit.SentenceSplitter.API/src/main/jcas/com/textocat/textokit/segmentation/fstype/Paragraph.java
+++ Textokit.SentenceSplitter.API/src/main/jcas/com/textocat/textokit/segmentation/fstype/Paragraph.java
@@ -40,3 +40,53 @@ public class Paragraph extends Annotation {
     /**
      * Never called.  Disable default constructor
      *
+     * @generated
+     */
+    protected Paragraph() {/* intentionally empty block */}
+
+    /**
+     * Internal - constructor used by generator
+     *
+     * @param addr low level Feature Structure reference
+     * @param type the type of this Feature Structure
+     * @generated
+     */
+    public Paragraph(int addr, TOP_Type type) {
+        super(addr, type);
+        readObject();
+    }
+
+    /**
+     * @param jcas JCas to which this Feature Structure belongs
+     * @generated
+     */
+    public Paragraph(JCas jcas) {
+        super(jcas);
+        readObject();
+    }
+
+    /**
+     * @param jcas  JCas to which this Feature Structure belongs
+     * @param begin offset to the begin spot in the SofA
+     * @param end   offset to the end spot in the SofA
+     * @generated
+     */
+    public Paragraph(JCas jcas, int begin, int end) {
+        super(jcas);
+        setBegin(begin);
+        setEnd(end);
+        readObject();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * Write your own initialization here
+     * <!-- end-user-doc -->
+     *
+     * @generated modifiable
+     */
+    private void readObject() {/*default - does nothing empty block */}
+
+}
+
+    
---
+++ Textokit.SentenceSplitter.API/src/main/jcas/com/textocat/textokit/segmentation/fstype/Sentence.java
@@ -0,0 +1,28 @@
+/* First created by JCasGen Thu Nov 12 17:05:14 MSK 2015 */
+package com.textocat.textokit.segmentation.fstype;
+
+import com.textocat.textokit.tokenizer.fstype.TokenBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/**
+ * Updated by JCasGen Thu Nov 12 17:05:14 MSK 2015
+ * XML source: src/main/resources/com/textocat/textokit/segmentation/segmentation-TypeSystem.xml
+ *
+ * @generated
+ */
+public class Sentence extends Annotation {
+    /**
+     * @generated
+     * @ordered
+     */
+    @SuppressWarnings("hiding")
+    public final static int typeIndexID = JCasRegistry.register(Sentence.class);
+    /**
+     * @generated
+     * @ordered
+     */
+    @SuppressWarn
