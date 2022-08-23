7e3d3537233a0d96435badbb863f55780c44559a
cinderellab <cinderela80@proton.me>
2022-08-23 07:32:07 +0000

    Add history

--- Textokit.SentenceSplitter.Heuristic/src/main/java/com/textocat/textokit/segmentation/heur/OneSentencePerLineSplitter.java
+++ Textokit.SentenceSplitter.Heuristic/src/main/java/com/textocat/textokit/segmentation/heur/OneSentencePerLineSplitter.java
@@ -36,4 +36,38 @@ import java.util.Deque;
 /**
  * @author Rinat Gareev
  */
-public cla
+public class OneSentencePerLineSplitter extends JCasAnnotator_ImplBase {
+
+    public static AnalysisEngineDescription createDescription() throws ResourceInitializationException {
+        TypeSystemDescription tsDesc = SentenceSplitterAPI.getTypeSystemDescription();
+        return AnalysisEngineFactory.createEngineDescription(
+                OneSentencePerLineSplitter.class, tsDesc);
+    }
+
+    @Override
+    public void process(JCas cas) throws AnalysisEngineProcessException {
+        Deque<Token> sentTokens = Lists.newLinkedList();
+        for (TokenBase tb : JCasUtil.select(cas, TokenBase.class)) {
+            if (tb instanceof BREAK) {
+                if (sentTokens.isEmpty()) {
+                    continue;
+                }
+                makeSentence(cas, sentTokens.getFirst(), sentTokens.getLast());
+                sentTokens.clear();
+            } else if (tb instanceof Token) {
+                sentTokens.add((Token) tb);
+            }
+        }
+        // make last sentence if any tokens are left
+        if (!sentTokens.isEmpty()) {
+            makeSentence(cas, sentTokens.getFirst(), sentTokens.getLast());
+        }
+    }
+
+    private void makeSentence(JCas cas, Token first, Token last) {
+        Sentence sent = new Sentence(cas, first.getBegin(), last.getEnd());
+        sent.setFirstToken(first);
+        sent.setLastToken(last);
+        sent.addToIndexes();
+    }
+}
