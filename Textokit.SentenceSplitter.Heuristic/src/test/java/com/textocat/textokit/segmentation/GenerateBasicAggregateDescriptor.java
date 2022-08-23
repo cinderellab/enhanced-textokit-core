fbfb674c743780a67f2ff1b528002a7180f0eb3a
cinderellab <cinderela80@proton.me>
2022-08-23 19:18:33 +0000

    Update backend

---
+++ Textokit.SentenceSplitter.Heuristic/src/test/java/com/textocat/textokit/segmentation/GenerateBasicAggregateDescriptor.java
@@ -0,0 +1,25 @@
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.segmentation;
+
+import com.google.common.collect.Maps;
+import com.textocat.textokit.commons.util.PipelineDescriptorUtils;
+import com.textocat.textokit.tokenizer.TokenizerAPI;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.a
