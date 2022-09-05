1f321aa7852ec98c91888c299eef2d3d0c763741
cinderellab <cinderela80@proton.me>
2022-09-05 16:14:33 +0000

    Comments update.

--- Textokit.SentenceSplitter.Heuristic/src/test/java/com/textocat/textokit/segmentation/GenerateOSPLSplitterDescriptor.java
+++ Textokit.SentenceSplitter.Heuristic/src/test/java/com/textocat/textokit/segmentation/GenerateOSPLSplitterDescriptor.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright 2015 Textocat
  *
@@ -16,40 +17,25 @@
 
 package com.textocat.textokit.segmentation;
 
-import com.google.common.collect.Maps;
-import com.textocat.textokit.commons.util.PipelineDescriptorUtils;
-import com.textocat.textokit.tokenizer.TokenizerAPI;
+import com.textocat.textokit.segmentation.heur.OneSentencePerLineSplitter;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.resource.metadata.MetaDataObject;
 import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * @author Rinat Gareev
  */
-public class GenerateBasicAggregateDescriptor {
-
-    /**
-     * @param args
-     * @throws ResourceInitializationException
-     */
+public class GenerateOSPLSplitterDescriptor {
     public static void main(String[] args) throws UIMAException, IOException, SAXException {
-        Map<String, MetaDataObject> aeDescriptions = Maps.newLinkedHashMap();
-        aeDescriptions.put("tokenizer", TokenizerAPI.getAEImport());
-
-        aeDescriptions.put("sentenceSplitter", SentenceSplitterAPI.getAEImport());
-
-        String outputPath = "desc/basic-aggregate.xml";
-        AnalysisEngineDescription desc = PipelineDescriptorUtils
-                .createAggregateDescription(aeDescriptions);
+        String outputPath = "src/main/resources/"
+                + OneSentencePerLineSplitter.class.getName().replace('.', '/') + ".xml";
+        AnalysisEngineDescription desc = OneSentencePerLineSplitter.createDescription();
         FileOutputStream out = FileUtils.openOutputStream(new File(outputPath));
         try {
             desc.toXML(out);
---
+++ Textokit.SentenceSplitter.Heuristic/src/test/java/com/textocat/textokit/segmentation/GenerateParagraphSplitterDescriptor.java
@@ -0,0 +1,31 @@
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.segmentation;
+
+import com.textocat.textokit.segmentation.heur.ParagraphSplitter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.apache.uima.fit.f
