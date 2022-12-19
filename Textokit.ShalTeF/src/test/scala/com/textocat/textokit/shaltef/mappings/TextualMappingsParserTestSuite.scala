510d1f53bfec417f5d23759fc692a9a3d87adeda
cinderellab <cinderela80@proton.me>
2022-12-19 22:30:24 +0000

    [#4343] Auto commit from MSBuild Deploy.

--- Textokit.ShalTeF/src/test/scala/com/textocat/textokit/shaltef/mappings/TextualMappingsParserTestSuite.scala
+++ Textokit.ShalTeF/src/test/scala/com/textocat/textokit/shaltef/mappings/TextualMappingsParserTestSuite.scala
@@ -40,4 +40,54 @@ class TextualMappingsParserTestSuite extends FunSuite with MockitoSugar with Cas
   test("Parse mappings in release.txt") {
     val morphDict = mock[MorphDictionary]
     val gramModel = mock[GramModel]
-    when(morphDict.getGramModel).thenRet
+    when(morphDict.getGramModel).thenReturn(gramModel)
+    // stub invocations for trigger lemma search
+    when(morphDict.getEntries("выпустил")).thenReturn(
+      Wordform.builder(gramModel, 100).build() :: Nil)
+    when(morphDict.getEntries("выйдет")).thenReturn(
+      Wordform.builder(gramModel, 200).build :: Nil)
+    // stub invocations for grammeme extractors
+    val caseBS = new BitSet
+    caseBS.set(100)
+    val gndrBS = new BitSet
+    caseBS.set(200)
+    when(gramModel.getGrammemWithChildrenBits("CAse", false)).thenReturn(caseBS)
+    when(gramModel.getGrammemWithChildrenBits("GNdr", false)).thenReturn(gndrBS)
+    when(gramModel.toGramSet(caseBS)).thenReturn(List("nomn", "accs", "ablt"))
+    when(gramModel.toGramSet(gndrBS)).thenReturn(List("masc", "femn", "neut"))
+
+    val constrValueFactory = new ConstraintValueFactory(gramModel)
+    val constrTargetFactory = new ConstraintTargetFactory(gramModel)
+    val constrFactory = new PhraseConstraintFactory
+    val parser = TextualMappingsParser(new MappingsParserConfig(morphDict))
+
+    val mappingsBuilder = DepToArgMappingsBuilder()
+    val templateAnnoType = ts.getType("test.Release")
+    assert(templateAnnoType != null, "Can't final Release annotation type")
+    parser.parse(new File("src/test/resources/mappings/release.txt").toURI.toURL,
+      templateAnnoType, mappingsBuilder)
+
+    val subjFeat = templateAnnoType.getFeatureByBaseName("subj")
+    val objFeat = templateAnnoType.getFeatureByBaseName("obj")
+    val dateFeat = templateAnnoType.getFeatureByBaseName("date")
+
+    val mappings = mappingsBuilder.build().asInstanceOf[DefaultDepToArgMappingsHolder]
+    assert(mappings.triggerLemmaId2Mappings.size === 2)
+
+    import constrFactory._
+    import constrTargetFactory._
+    import constrValueFactory._
+    val pattern1 = new ConstraintConjunctionPhrasePattern(
+      phraseConstraint(headFeature("case"), Equals, constant("nomn")) ::
+        phraseConstraint(headFeature("gndr"), Equals, triggerFeatureReference("gndr")) ::
+        Nil)
+    val pattern2 = new ConstraintConjunctionPhrasePattern(
+      phraseConstraint(headFeature("case"), Equals, constant("accs"))
+        :: Nil)
+    val pattern3 = new ConstraintConjunctionPhrasePattern(
+      phraseConstraint(prepositionTarget, Equals, constant("в"))
+        :: Nil)
+    assert(mappings.triggerLemmaId2Mappings(100) === new DefaultDepToArgMapping(
+      templateAnnoType, Set(100),
+      SlotMapping(pattern1, false, Some(subjFeat)) ::
+       
