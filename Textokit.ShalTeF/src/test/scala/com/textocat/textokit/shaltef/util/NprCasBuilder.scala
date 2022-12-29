4e915d00bc0ad54d492d76e2c12f715c0483f34b
cinderellab <cinderela80@proton.me>
2022-12-29 19:08:26 +0000

    powerpc/pseries: Fix software invalidate TCE

--- Textokit.ShalTeF/src/test/scala/com/textocat/textokit/shaltef/util/NprCasBuilder.scala
+++ Textokit.ShalTeF/src/test/scala/com/textocat/textokit/shaltef/util/NprCasBuilder.scala
@@ -23,4 +23,76 @@ import com.textocat.textokit.morph.fs.{Word, Wordform}
 import com.textocat.textokit.phrrecog.cas.{NounPhrase, Phrase}
 import com.textocat.textokit.segmentation.SentenceSplitterAPI
 import com.textocat.textokit.segmentation.fstype.Sentence
-import com.textocat.texto
+import com.textocat.textokit.tokenizer.TokenizerAPI
+import com.textocat.textokit.tokenizer.fstype.W
+import org.apache.uima.cas.impl.XmiCasSerializer
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory._
+import org.apache.uima.fit.util.FSCollectionFactory._
+import org.apache.uima.jcas.JCas
+import org.apache.uima.jcas.cas.StringArray
+import org.apache.uima.util.CasCreationUtils
+
+import scala.collection.JavaConversions.{asJavaCollection, iterableAsScalaIterable}
+import scala.collection.mutable
+
+/**
+ * @author Rinat Gareev
+ */
+class NprCasBuilder(val text: String, additionalTypeSystemNames: List[String]) {
+
+  val ts = {
+    val tsNames = "com.textocat.textokit.phrrecog.ts-phrase-recognizer" ::
+      TokenizerAPI.TYPESYSTEM_TOKENIZER ::
+      SentenceSplitterAPI.TYPESYSTEM_SENTENCES ::
+      additionalTypeSystemNames
+    val tsDesc = createTypeSystemDescription(tsNames: _*)
+    val dumbCas = CasCreationUtils.createCas(tsDesc, null, null)
+    dumbCas.getTypeSystem
+  }
+  val cas = CasCreationUtils.createCas(ts, null, null, null)
+  cas.setDocumentText(text)
+  val jCas = cas.getJCas
+
+  private val wfMap = mutable.Map.empty[String, Wordform]
+
+  def w(begin: Int, end: Int): Wordform = w(text.substring(begin, end), begin, end)
+
+  def w(id: String, begin: Int, end: Int): Wordform = {
+    require(!wfMap.contains(id), "Duplicate id: %s".format(id))
+    val token = new W(jCas)
+    token.setBegin(begin)
+    token.setEnd(end)
+    token.addToIndexes()
+    val word = new Word(jCas)
+    word.setBegin(begin)
+    word.setEnd(end)
+    word.setToken(token)
+
+    val wf = new Wordform(jCas)
+    wf.setWord(word)
+    word.setWordforms(FSUtils.toFSArray(jCas, wf))
+
+    word.addToIndexes()
+
+    wfMap(id) = wf
+    wf
+  }
+
+  def w(id: String): Wordform =
+    if (id == null) null
+    else wfMap(id)
+
+  def np(headId: String, prepId: String = null, particleId: String = null,
+         depWordIds: Iterable[String] = Nil, depNPs: Iterable[Phrase] = Nil,
+         index: Boolean = false): NounPhrase = {
+    val npAnno = new NounPhrase(jCas)
+    val head = w(headId)
+    npAnno.setBegin(head.getWord.getBegin)
+    npAnno.setEnd(head.getWord.getEnd)
+    npAnno.setHead(head)
+    npAnno.setParticle(w(particleId))
+    npAnno.setPreposition(w(prepId))
+    if (depWordIds.nonEmpty)
+      npAnno.setDependentWords(createFSArray(jCas, depWordIds.map(w)))
+    if (depNPs.nonEmpty)
+      npAnno.setDependentPhrases(createFSArray(jCas, de
