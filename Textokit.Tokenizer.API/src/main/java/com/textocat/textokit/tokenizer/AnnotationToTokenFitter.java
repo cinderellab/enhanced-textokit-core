afc55d444232b1b28fc224faf8aa96cb01fd4a98
cinderellab <cinderela80@proton.me>
2023-02-06 20:42:18 +0000

    alert

--- Textokit.Tokenizer.API/src/main/java/com/textocat/textokit/tokenizer/AnnotationToTokenFitter.java
+++ Textokit.Tokenizer.API/src/main/java/com/textocat/textokit/tokenizer/AnnotationToTokenFitter.java
@@ -26,4 +26,60 @@ import com.textocat.textokit.commons.cas.OverlapIndex;
 import com.textocat.textokit.commons.util.Offsets;
 import com.textocat.textokit.tokenizer.fstype.Token;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.analy
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+import static com.textocat.textokit.commons.cas.AnnotationUtils.createOverlapIndex;
+import static com.textocat.textokit.commons.cas.AnnotationUtils.toPrettyString;
+import static java.lang.String.format;
+
+/**
+ * TODO test
+ *
+ * @author Rinat Gareev
+ */
+public class AnnotationToTokenFitter<ST extends Annotation> extends JCasAnnotator_ImplBase {
+
+    public static AnalysisEngineDescription createDescription(
+            Class<? extends Annotation> targetTypeClass) throws ResourceInitializationException {
+        return AnalysisEngineFactory.createEngineDescription(AnnotationToTokenFitter.class,
+                PARAM_TARGET_TYPE, targetTypeClass.getName());
+    }
+
+    public static final String PARAM_TARGET_TYPE = "targetType";
+
+    @ConfigurationParameter(name = PARAM_TARGET_TYPE, mandatory = true)
+    private Class<ST> targetTypeClass;
+
+    @Override
+    public void process(JCas jCas) throws AnalysisEngineProcessException {
+        Set<ST> anno2Remove = Sets.newLinkedHashSet();
+        Map<ST, Offsets> anno2Fix = Maps.newLinkedHashMap();
+        OverlapIndex<Token> tokenOverlapIndex =
+                createOverlapIndex(JCasUtil.select(jCas, Token.class).iterator());
+        //
+        for (ST span : JCasUtil.select(jCas, targetTypeClass)) {
+            Offsets spanOffsets = new Offsets(span);
+            LinkedList<Token> overlappingTokens = Lists.newLinkedList(
+                    tokenOverlapIndex.getOverlapping(spanOffsets.getBegin(), spanOffsets.getEnd()));
+            if (overlappingTokens.isEmpty()) {
+                getLogger().warn(format("No overlapping tokens for typed span: %s", toPrettyString(span)));
+                anno2Remove.add(span);
+                continue;
+            }
+            // OT stands for 'overlapping token'
+            Token firstOT = overlappingTokens.getFirst();
+            Token lastOT = overlappingTokens.getLast();
+            // check the first overlapping token
+            if (firstOT.getBegin() != spanOffsets.getBegin()) {
+                spanOffsets 
