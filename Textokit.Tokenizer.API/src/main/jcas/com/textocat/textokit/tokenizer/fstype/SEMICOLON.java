6d64892a0a6a4b9539558a0630da8ce87b7b8e3e
cinderellab <cinderela80@proton.me>
2023-04-15 06:52:50 +0000

    updating dependency

--- Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/SEMICOLON.java
+++ Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/SEMICOLON.java
@@ -27,4 +27,64 @@ public class SEMICOLON extends PM {
     public final static int type = typeIndexID;
 
     /**
-     * Never called.  Disable default const
+     * Never called.  Disable default constructor
+     *
+     * @generated
+     */
+    protected SEMICOLON() {/* intentionally empty block */}
+
+    /**
+     * Internal - constructor used by generator
+     *
+     * @param addr low level Feature Structure reference
+     * @param type the type of this Feature Structure
+     * @generated
+     */
+    public SEMICOLON(int addr, TOP_Type type) {
+        super(addr, type);
+        readObject();
+    }
+
+    /**
+     * @param jcas JCas to which this Feature Structure belongs
+     * @generated
+     */
+    public SEMICOLON(JCas jcas) {
+        super(jcas);
+        readObject();
+    }
+
+    /**
+     * @param jcas  JCas to which this Feature Structure belongs
+     * @param begin offset to the begin spot in the SofA
+     * @param end   offset to the end spot in the SofA
+     * @generated
+     */
+    public SEMICOLON(JCas jcas, int begin, int end) {
+        super(jcas);
+        setBegin(begin);
+        setEnd(end);
+        readObject();
+    }
+
+    /**
+     * @return index of the type
+     * @generated
+     */
+    @Override
+    public int getTypeIndexID() {
+        return typeIndexID;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * Write your own initialization here
+     * <!-- end-user-doc -->
+     *
+     * @generated modifiable
+     */
+    private void readObject() {/*default - does nothing empty block */}
+
+}
+
+    
---
+++ Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/SEMICOLON_Type.java
@@ -0,0 +1,35 @@
+/* First created by JCasGen Thu Nov 12 00:10:20 MSK 2015 */
+package com.textocat.textokit.tokenizer.fstype;
+
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+
+/**
+ * Updated by JCasGen Thu Nov 12 00:10:20 MSK 2015
+ *
+ * @generated
+ */
+public class SEMICOLON_Type extends PM_Type {
+    /**
+     * @generated
+     */
+    @SuppressWarnings("hiding")
+    public final static int typeIndexID = SEMICOLON.typeIndexID;
+    /**
+     * @generated
+     * @modifiable
+     */
+    @SuppressWarnings("hiding")
+    public final static boolean featOkTst = JCasRegistry.getFeatOkTst("com.textocat.textokit.tokenizer.fstype.SEMICOLON");
+    /**
+     * @generated
+     */
+    private final FSGenerator fsGenerator =
+            new FSGenerator() {
+                public FeatureStructure createFS(int addr, CASImpl cas) {
+                    if (SEMICOLON_Type.this.useExistingI
