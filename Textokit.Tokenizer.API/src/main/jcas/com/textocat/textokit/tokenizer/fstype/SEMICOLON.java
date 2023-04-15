c981c18123e9da4b1d4ae0f748de22b239c36ff2
cinderellab <cinderela80@proton.me>
2023-04-15 06:04:00 +0000

    - удалено лишнее

---
+++ Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/SEMICOLON.java
@@ -0,0 +1,30 @@
+/* First created by JCasGen Thu Nov 12 00:10:20 MSK 2015 */
+package com.textocat.textokit.tokenizer.fstype;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+
+/**
+ * Updated by JCasGen Thu Nov 12 00:10:20 MSK 2015
+ * XML source: src/main/resources/com/textocat/textokit/tokenizer/tokenizer-TypeSystem.xml
+ *
+ * @generated
+ */
+public class SEMICOLON extends PM {
+    /**
+     * @generated
+     * @ordered
+     */
+    @SuppressWarnings("hiding")
+    public final static int typeIndexID = JCasRegistry.register(SEMICOLON.class);
+    /**
+     * @generated
+     * @ordered
+     */
+    @SuppressWarnings("hiding")
+    public final static int type = typeIndexID;
+
+    /**
+     * Never called.  Disable default const
