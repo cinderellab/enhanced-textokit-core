a311716eb5e46ce95822760335e0d86d0e3b2596
cinderellab <cinderela80@proton.me>
2023-04-29 08:27:46 +0000

    Fix #47

--- Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/Token_Type.java
+++ Textokit.Tokenizer.API/src/main/jcas/com/textocat/textokit/tokenizer/fstype/Token_Type.java
@@ -8,66 +8,44 @@ import org.apache.uima.cas.impl.FSGenerator;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JCasRegistry;
-import org.apache.uima.jcas.tcas.Annotation_Type;
 
 /**
  * Updated by JCasGen Thu Nov 12 00:10:20 MSK 2015
  *
  * @generated
  */
-public class TokenBase_Type extends Annotation_Type {
+public class Token_Type extends TokenBase_Type {
     /**
      * @generated
      */
     @SuppressWarnings("hiding")
-    public final static int typeIndexID = TokenBase.typeIndexID;
+    public final static int typeIndexID = Token.typeIndexID;
     /**
      * @generated
      * @modifiable
      */
     @SuppressWarnings("hiding")
-    public final static boolean featOkTst = JCasRegistry.getFeatOkTst("com.textocat.textokit.tokenizer.fstype.TokenBase");
+    public final static boolean featOkTst = JCasRegistry.getFeatOkTst("com.textocat.textokit.tokenizer.fstype.Token");
     /**
      * @generated
      */
     private final FSGenerator fsGenerator =
             new FSGenerator() {
                 public FeatureStructure createFS(int addr, CASImpl cas) {
-                    if (TokenBase_Type.this.useExistingInstance) {
+                    if (Token_Type.this.useExistingInstance) {
                         // Return eq fs instance if already created
-                        FeatureStructure fs = TokenBase_Type.this.jcas.getJfsFromCaddr(addr);
+                        FeatureStructure fs = Token_Type.this.jcas.getJfsFromCaddr(addr);
                         if (null == fs) {
-                            fs = new TokenBase(addr, TokenBase_Type.this);
-                            TokenBase_Type.this.jcas.putJfsFromCaddr(addr, fs);
+                            fs = new Token(addr, Token_Type.this);
+                            Token_Type.this.jcas.putJfsFromCaddr(addr, fs);
                             return fs;
                         }
                         return fs;
-                    } else return new TokenBase(addr, TokenBase_Type.this);
+                    } else return new Token(addr, Token_Type.this);
                 }
             };
     /**
      * initialize variables to correspond with Cas Type and Features
      *
      * @param jcas    JCas
-     * @param casType Type
-     * @generated
-     */
-    public TokenBase_Type(JCas jcas, Type casType) {
-        super(jcas, casType);
-        casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl) this.casType, getFSGenerator());
-
-    }
-
-    /**
-     * @return the generator for this type
-     * @generated
-     */
-    @Override
-    protected FSGenerator getFSGenerator() {
-        return fsGenerator;
-    }
-}
-
-
-
-    
+  
