b7cba37a6d06529d2faaef8d67906543e7107fdb
cinderellab <cinderela80@proton.me>
2023-06-08 18:44:16 +0000

    Icon updates

--- Textokit.Tokenizer.Simple/src/test/java/com/textocat/textokit/tokenizer/simple/GeneratePostTokenizerDescriptor.java
+++ Textokit.Tokenizer.Simple/src/test/java/com/textocat/textokit/tokenizer/simple/GeneratePostTokenizerDescriptor.java
@@ -9,4 +9,39 @@
  *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES 
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.textocat.textokit.tokenizer.simple;
+
+import com.textocat.textokit.tokenizer.TokenizerAPI;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.xml.sax.SAXException;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+/**
+ * @author Rinat Gareev
+ */
+public class GeneratePostTokenizerDescriptor {
+
+    public static void main(String[] args) throws UIMAException, IOException, SAXException {
+        String outputPath = "src/main/resources/com/textocat/textokit/tokenizer/simple/PostTokenizer.xml";
+        TypeSystemDescription tsDesc = TokenizerAPI.getTypeSystemDescription();
+        AnalysisEngineDescription desc = createEngineDescription(PostTokenizer.class, tsDesc);
+        FileOutputStream out = new FileOutputStream(outputPath);
+        try {
+            desc.toXML(out);
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
+    }
+}
